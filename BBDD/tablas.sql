03e2db9e3dfb1f47d19edc3294378e698c7365a0
Alejandro <alex.garcia.2002@hotmail.com>
2023-10-11 09:50:04 +0200

    Cambios base datos y plantilla

--- BBDD/tablas.sql
+++ BBDD/tablas.sql
@@ -1,25 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-10-2023 a las 09:37:24
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `buscaminas`
+--
 
-CREATE TABLE usuarios (
-    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    correo VARCHAR(255) NOT NULL UNIQUE,
-    pass VARCHAR(255) NOT NULL,
-    esAdmin INT DEFAULT 0,
-    partidasJugadas INT DEFAULT 0,
-    partidasGanadas INT DEFAULT 0
-);
+-- --------------------------------------------------------
 
-CREATE TABLE partida (
-    idPartida INT AUTO_INCREMENT PRIMARY KEY,
-    idPersona INT NOT NULL,
-    tablaOculta VARCHAR(255),
-    tablaJugador VARCHAR(255),
-    finalizada INT DEFAULT 0  
-);
+--
+-- Estructura de tabla para la tabla `partida`
+--
 
+CREATE TABLE `partida` (
+  `idPartida` int(11) NOT NULL,
+  `idPersona` int(11) NOT NULL,
+  `tablaOculta` varchar(255) DEFAULT NULL,
+  `tablaJugador` varchar(255) DEFAULT NULL,
+  `finalizada` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into usuarios (idUsuario,nombre,correo,pass,esAdmin,partidasJugadas,partidasGanadas) VALUES 
-(DEFAULT,"Alejandro","alejandro","1234",1,DEFAULT,DEFAULT); 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `esAdmin` int(11) DEFAULT 0,
+  `partidasJugadas` int(11) DEFAULT 0,
+  `partidasGanadas` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `nombre`, `correo`, `pass`, `esAdmin`, `partidasJugadas`, `partidasGanadas`) VALUES
+(1, 'Alejandro', 'alejandro', '1234', 1, 0, 0),
+(2, 'Laura', 'laura', '1234', 0, 0, 0),
+(7, 'Charlex', 'charlex', '1234', 0, 0, 0),
+(8, 'Fornell', 'fornell', '1234', 0, 0, 0),
+(9, 'Gonzalo', 'gonzalo', '1234', 0, 0, 0),
+(10, 'Juanra', 'juanra', '1234', 0, 0, 0),
+(11, 'Chechu', 'chechu', '1234', 0, 0, 0),
+(12, 'Guille', 'guille', '1234', 0, 0, 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `partida`
+--
+ALTER TABLE `partida`
+  ADD PRIMARY KEY (`idPartida`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `partida`
+--
+ALTER TABLE `partida`
+  MODIFY `idPartida` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
